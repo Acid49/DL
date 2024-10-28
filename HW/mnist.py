d6ceddee90f65e7c8a6994708b824657d541b29c
Acid49 <67170582+Acid49@users.noreply.github.com>
2024-10-29 01:09:27 +0900

    using gzip

--- HW/mnist.py
+++ HW/mnist.py
@@ -1,25 +1,30 @@
 import numpy as np
+import gzip
 
 def load_data():
-    train_labels_path = 'train-labels-idx1-ubyte'
-    train_images_path = 'train-images-idx3-ubyte'
-    test_labels_path = 't10k-labels-idx1-ubyte'
-    test_images_path = 't10k-images-idx3-ubyte'
+    train_labels_path = 'train-labels-idx1-ubyte.gz'
+    train_images_path = 'train-images-idx3-ubyte.gz'
+    test_labels_path = 't10k-labels-idx1-ubyte.gz'
+    test_images_path = 't10k-images-idx3-ubyte.gz'
 
-    with open(train_labels_path, 'rb') as lbpath:
-        lbpath.read(8)
-        train_labels = np.fromfile(lbpath, dtype=np.uint8)
+    with gzip.open(train_labels_path, 'rb') as f:
+        f.read(8)
+        buf = f.read()
+        train_labels = np.frombuffer(buf, dtype=np.uint8)
 
-    with open(train_images_path, 'rb') as imgpath:
-        imgpath.read(16)
-        train_images = np.fromfile(imgpath, dtype=np.uint8).reshape(-1, 28, 28)
+    with gzip.open(train_images_path, 'rb') as f:
+        f.read(16)
+        buf = f.read()
+        train_images = np.frombuffer(buf, dtype=np.uint8).reshape(-1, 28, 28)
 
-    with open(test_labels_path, 'rb') as lbpath:
-        lbpath.read(8)
-        test_labels = np.fromfile(lbpath, dtype=np.uint8)
+    with gzip.open(test_labels_path, 'rb') as f:
+        f.read(8)
+        buf = f.read()
+        test_labels = np.frombuffer(buf, dtype=np.uint8)
 
-    with open(test_images_path, 'rb') as imgpath:
-        imgpath.read(16)
-        test_images = np.fromfile(imgpath, dtype=np.uint8).reshape(-1, 28, 28)
+    with gzip.open(test_images_path, 'rb') as f:
+        f.read(16)
+        buf = f.read()
+        test_images = np.frombuffer(buf, dtype=np.uint8).reshape(-1, 28, 28)
 
     return (train_images, train_labels), (test_images, test_labels)
